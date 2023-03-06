d442e10dda8ed1711b41b62253e2343e1a27ca77
CBIS2022oscar <123550530+CBIS2022oscar@users.noreply.github.com>
2023-03-06 15:58:22 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 input.onButtonPressed(Button.A, function () {
-    music.setTempo(122)
+    music.setTempo(154)
     for (let index = 0; index < 50; index++) {
         music.rest(music.beat(BeatFraction.Breve))
         if (input.lightLevel() < 90) {
-            music.playTone(392, music.beat(BeatFraction.Half))
+            music.playTone(392, music.beat(BeatFraction.Whole))
             basic.showLeds(`
                 # . . . #
                 . # . # .
@@ -13,16 +13,43 @@ input.onButtonPressed(Button.A, function () {
                 `)
         } else {
             if (input.lightLevel() < 30) {
-                music.playTone(523, music.beat(BeatFraction.Breve))
+                music.playTone(523, music.beat(BeatFraction.Whole))
                 basic.showLeds(`
                     # . # . #
-                    . . . . .
+                    . # . # .
                     # # # # #
-                    . . . . .
+                    . # . # .
                     # . # . #
                     `)
             } else {
-                music.playTone(370, music.beat(BeatFraction.Breve))
+                music.playTone(415, music.beat(BeatFraction.Whole))
+                basic.showLeds(`
+                    # . . . #
+                    . # . # .
+                    # . . . #
+                    . # # # .
+                    # . . . #
+                    `)
+            }
+            if (input.lightLevel() < 50) {
+                music.playTone(659, music.beat(BeatFraction.Whole))
+                basic.showLeds(`
+                    . # . . #
+                    . . . . .
+                    . . # # .
+                    . # . . #
+                    . . . . .
+                    `)
+            }
+            if (input.lightLevel() < 65) {
+                music.playTone(165, music.beat(BeatFraction.Whole))
+                basic.showLeds(`
+                    # . # . #
+                    # # # # #
+                    # . . . #
+                    . # . # .
+                    . . # . .
+                    `)
             }
         }
     }
